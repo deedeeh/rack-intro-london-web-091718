6b87770453c37b0ec7a89c560c8eddfed99a8966
deedeeh <dinahafez.8@gmail.com>
2018-10-06 13:14:44 +0100

    Added more response messages and tested it

--- application.rb
+++ application.rb
@@ -2,9 +2,10 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World!\n"
+    resp.write "My name is Dina\n"
+    resp.write "I can't wait to learn more about Rack & Rails!"
     resp.finish
   end
 
 end
-
